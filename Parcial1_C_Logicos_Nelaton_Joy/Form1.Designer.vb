55764b99f513c8354e17972df54dfdc19a8a817e
Joy <joy.nelaton@utp.ac.pa>
2024-04-25 21:32:15 -0500

    Commit_01

---
+++ Parcial1_C_Logicos_Nelaton_Joy/Form1.Designer.vb
@@ -0,0 +1,388 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Parcial_1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(307, 224)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Sistemas Numericos"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Parcial1_C_Logicos_Nelaton_Joy.My.Resources.Resources.a
+        Me.PictureBox1.Location = New System.Drawing.Point(16, 33)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(276, 167)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Location = New System.Drawing.Point(325, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(273, 224)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Decimal a otros Sistemas"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(18, 40)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(91, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Valor en decimal: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 134)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(108, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Valor en hexadecimal"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Valor en octal"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 70)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Valor en binario"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(115, 33)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(52, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "0"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(89, 173)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label7)
+        Me.GroupBox3.Controls.Add(Me.TextBox3)
+        Me.GroupBox3.Controls.Add(Me.TextBox2)
+        Me.GroupBox3.Controls.Add(Me.Label6)
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.Button2)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 268)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(273, 224)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Resta Binaria por C2"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(16, 143)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(121, 13)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Resultado Resta Binaria"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(76, 93)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(53, 20)
+        Me.TextBox3.TabIndex = 4
+        Me.TextBox3.Text = "0"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(76, 52)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(53, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(13, 96)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(61, 13)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "Sustraendo"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(16, 59)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(54, 13)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Minuendo"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(92, 175)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "Calcular"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Button3)
+        Me.GroupBox4.Controls.Add(Me.Label9)
+        Me.GroupBox4.Controls.Add(Me.TextBox4)
+        Me.GroupBox4.Controls.Add(Me.Label8)
+        Me.GroupBox4.Location = New System.Drawing.Point(325, 268)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(273, 224)
+        Me.GroupBox4.TabIndex = 3
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Codigo Gray"
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.Button4)
+        Me.GroupBox5.Controls.Add(Me.DataGridView1)
+        Me.GroupBox5.Controls.Add(Me.TextBox6)
+        Me.GroupBox5.Controls.Add(Me.Label12)
+        Me.GroupBox5.Controls.Add(Me.TextBox5)
+        Me.GroupBox5.Controls.Add(Me.Label10)
+        Me.GroupBox5.Location = New System.Drawing.Point(616, 12)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(345, 480)
+        Me.GroupBox5.TabIndex = 4
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Redundancia Ciclica"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(18, 52)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(39, 13)
+        Me.Label8.TabIndex = 2
+        Me.Label8.Text = "Binario"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(78, 49)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(59, 20)
+        Me.TextBox4.TabIndex = 4
+        Me.TextBox4.Text = "0"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(18, 96)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(80, 13)
+        Me.Label9.TabIndex = 5
+        Me.Label9.Text = "Resultado Gray"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(62, 175)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Calcular"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(22, 48)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(62, 13)
+        Me.Label10.TabIndex = 3
+        Me.Label10.Text = "Informacion"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(90, 45)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(59, 20)
+        Me.TextBox5.TabIndex = 5
+        Me.TextBox5.Text = "0"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(22, 87)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(52, 13)
+        Me.Label12.TabIndex = 7
+        Me.Label12.Text = "Polinomio"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(90, 84)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(59, 20)
+        Me.TextBox6.TabIndex = 8
+        Me.TextBox6.Text = "0"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 129)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(333, 283)
+        Me.DataGridView1.TabIndex = 9
+        Me.DataGridView1.Visible = False
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(131, 431)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Calcular"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Parcial_1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(973, 523)
+        Me.Controls.Add(Me.GroupBox5)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.MaximizeBox = False
+        Me.Name = "Parcial_1"
+        Me.Text = "Parcial_1_C_Logicos_Nelaton_Joy"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox5.ResumeLayout(False)
+        Me.GroupBox5.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox5 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+
+End Class
---
+++ Parcial1_C_Logicos_Nelaton_Joy/Form1.vb
@@ -0,0 +1,256 @@
+﻿Public Class Parcial_1
+
+    
+
+    Private Function DecimalABinario(ByVal numero As Integer) As String
+        Dim resto As String = ""
+
+        Do Until numero < 2
+            resto = resto & Str(numero Mod 2)
+            numero = Int(numero / 2)
+        Loop
+
+        resto = resto & numero
+
+        Return StrReverse(resto)
+    End Function
+
+    Private Function DecimalAOctal(ByVal numero As Integer) As String
+        Dim resto As String = ""
+
+        Do Until numero < 8
+            resto = resto & Str(numero Mod 8)
+            numero = Int(numero / 8)
+        Loop
+
+        resto = resto & numero
+
+        Return StrReverse(resto)
+    End Function
+
+    Private Function DecimalAHexadecimal(ByVal numero As Integer) As String
+        Dim resto As String = ""
+        Dim hexDigits As String = "0123456789ABCDEF"
+
+        Do Until numero < 16
+            resto = resto & hexDigits.Chars(numero Mod 16)
+            numero = Int(numero / 16)
+        Loop
+
+        resto = resto & hexDigits.Chars(numero)
+
+        Return StrReverse(resto)
+    End Function
+
+    Private Sub ConvertirADecimal()
+        If TextBox1.Text = "" Or TextBox1.Text <= 0 Then
+            MessageBox.Show("Por favor valide el valor ingresado")
+        Else
+            Dim numero As Integer = TextBox1.Text
+
+            Label1.Text = "Valor en binario: " + DecimalABinario(numero)
+            Label2.Text = "Valor en octal: " + DecimalAOctal(numero)
+            Label3.Text = "Valor en hexadecimal: " + DecimalAHexadecimal(numero)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        ConvertirADecimal()
+    End Sub
+
+
+
+    Private Function EsBinario(ByVal valor As String) As Boolean
+        For Each caracter As Char In valor
+            If Not (caracter = "0" Or caracter = "1") Then
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+
+    Private Function ComplementoA2(ByVal binario As String) As String
+        Dim complemento As String = ""
+        Dim carry As Boolean = True
+
+        For i As Integer = binario.Length - 1 To 0 Step -1
+            If binario(i) = "1" And carry Then
+                complemento = "0" & complemento
+            ElseIf binario(i) = "0" And carry Then
+                complemento = "1" & complemento
+                carry = False
+            Else
+                complemento = binario(i) & complemento
+            End If
+        Next
+
+        Return complemento
+    End Function
+
+    Private Function RestaBinaria(ByVal minuendo As String, ByVal sustraendo As String) As String
+        Dim resultado As String = ""
+        Dim complementoSustraendo As String = ComplementoA2(sustraendo)
+
+        For i As Integer = minuendo.Length - 1 To 0 Step -1
+            If minuendo(i) = sustraendo(i) Then
+                resultado = "0" & resultado
+            Else
+                resultado = "1" & resultado
+            End If
+        Next
+
+        Return resultado
+    End Function
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If Not EsBinario(TextBox2.Text) Then
+            MessageBox.Show("Por favor, ingrese solo números binarios en el minuendo.")
+            Return
+        End If
+
+        If Not EsBinario(TextBox3.Text) Then
+            MessageBox.Show("Por favor, ingrese solo números binarios en el sustraendo.")
+            Return
+        End If
+
+        Dim minuendo As String = TextBox2.Text
+        Dim sustraendo As String = TextBox3.Text
+
+        ' Asegurarse de que ambos números tienen la misma longitud
+        Dim longitudMaxima As Integer = Math.Max(minuendo.Length, sustraendo.Length)
+        minuendo = minuendo.PadLeft(longitudMaxima, "0"c)
+        sustraendo = sustraendo.PadLeft(longitudMaxima, "0"c)
+
+        ' Realizar la resta binaria utilizando el complemento a 2 del sustraendo
+        Dim resultado As String = RestaBinaria(minuendo, sustraendo)
+
+        Label7.Text = "Resultado de la resta binaria: " & resultado
+    End Sub
+    
+
+    Private Function EsBinarioDe5Bits(ByVal valor As String) As Boolean
+        If valor.Length > 5 Then
+            Return False
+        End If
+
+        For Each caracter As Char In valor
+            If Not (caracter = "0" Or caracter = "1") Then
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+
+
+    Private Function BinarioAGray(ByVal binario As String) As String
+        Dim gray As String = binario(0)
+
+        For i As Integer = 1 To binario.Length - 1
+            If binario(i) <> binario(i - 1) Then
+                gray &= "1"
+            Else
+                gray &= "0"
+            End If
+        Next
+
+        Return gray
+    End Function
+    
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim binario As String = TextBox4.Text
+
+        If Not EsBinarioDe5Bits(binario) Then
+            MessageBox.Show("Por favor, ingrese un número binario de máximo 5 bits.")
+            Return
+        End If
+
+        Dim gray As String = BinarioAGray(binario)
+        Label9.Text = "Código de Gray equivalente: " & gray
+    End Sub
+
+
+    Private Function EsBinarioDe6Bits(ByVal valor As String) As Boolean
+        If valor.Length > 6 Then
+            Return False
+        End If
+
+        For Each caracter As Char In valor
+            If Not (caracter = "0" Or caracter = "1") Then
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+
+    Private Function XorBit(ByVal bit1 As Char, ByVal bit2 As Char) As Char
+        If bit1 = bit2 Then
+            Return "0"
+        Else
+            Return "1"
+        End If
+    End Function
+
+    Private Function CambiarBit(ByVal cadena As String, ByVal indice As Integer, ByVal bit As Char) As String
+        Dim arreglo As Char() = cadena.ToCharArray()
+        arreglo(indice) = bit
+        Return New String(arreglo)
+    End Function
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        DataGridView1.Visible = True
+        Dim mensaje As String = TextBox5.Text
+        Dim polinomioGenerador As String = TextBox6.Text
+
+        If Not EsBinarioDe6Bits(mensaje) Then
+            MessageBox.Show("Por favor, ingrese un mensaje binario de máximo 6 bits.")
+            Return
+        End If
+
+        If Not EsBinarioDe6Bits(polinomioGenerador) Then
+            MessageBox.Show("Por favor, ingrese un polinomio generador binario de máximo 6 bits.")
+            Return
+        End If
+
+        Dim mensajeExtendido As String = mensaje.PadRight(mensaje.Length + polinomioGenerador.Length - 1, "0")
+        Dim residuo As String = mensajeExtendido
+        Dim divisor As String = polinomioGenerador
+
+        Dim pasoAPaso As New List(Of String())
+
+        For i As Integer = 0 To mensaje.Length - 1
+            If residuo(i) = "1" Then
+                Dim paso As String = residuo
+                For j As Integer = 0 To divisor.Length - 1
+                    residuo = CambiarBit(residuo, i + j, XorBit(residuo(i + j), divisor(j)))
+                Next
+                pasoAPaso.Add({paso, divisor.PadLeft(i, "0"c)})
+            End If
+        Next
+
+        Dim resultado As String = residuo.Substring(mensaje.Length)
+
+        Dim pasoFinal As String = residuo
+        For i As Integer = 0 To polinomioGenerador.Length - 2
+            pasoFinal = CambiarBit(pasoFinal, mensaje.Length + i, "0"c)
+        Next
+        pasoAPaso.Add({pasoFinal, ""})
+
+        ' Llenar el DataGridView con los pasos y el resultado del CRC
+        DataGridView1.Rows.Clear()
+        DataGridView1.ColumnCount = 2
+        DataGridView1.Columns(0).Name = "Paso"
+        DataGridView1.Columns(1).Name = "Divisor"
+        For Each paso In pasoAPaso
+            DataGridView1.Rows.Add(paso)
+        Next
+        DataGridView1.Rows.Add({"Resultado", resultado})
+    End Sub
+
+
+
+
+End Class
+
+
+
